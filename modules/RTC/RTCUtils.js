57b76bb819edad0f1dc70ab75dfdb0154253fdb0
hristoterezov <hristo@jitsi.org>
2018-02-15 13:34:14 -0600

    fix(SS): Set min and max frame rate for the new GUM flow.

--- modules/RTC/RTCUtils.js
+++ modules/RTC/RTCUtils.js
@@ -462,7 +462,8 @@ function newGetConstraints(um = [], options = {}) {
                 chromeMediaSourceId: options.desktopStream,
                 maxWidth: window.screen.width,
                 maxHeight: window.screen.height,
-                maxFrameRate: 3
+                maxFrameRate: SS_FRAME_RATE,
+                minFrameRate: SS_FRAME_RATE
             }
         };
     }
